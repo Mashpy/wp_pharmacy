282ca552eda8de0a681ce3acc06880263b9928ce
unknown <Mashpy@Mashpy-PC.(none)>
2014-06-13 15:31:09 +0600

    updated the footer

--- footer.php
+++ footer.php
@@ -14,11 +14,36 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
-			<?php get_sidebar( 'footer' ); ?>
-
 			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
+			
+			
+				<div class="inner">
+					<div class="left">
+					
+						
+							<a href="http://www.nus.edu.sg/terms.php" >Terms of Use</a> | <a href="http://www.nus.edu.sg/policy.php" >Privacy</a> | <a href="http://www.nus.edu.sg/nondiscrim.php" >Non-discrimination</a>
+							<!-- | <a href="#">Using this site</a>-->
+						
+					</br>
+							&copy; Copyright 2001-2009 National University of Singapore. All Rights Reserved.
+					
+					</div>
+					<div class="right">
+							<a href="http://www.nus.edu.sg/sitemap.php" >Site Map</a> | <a href="http://www.nus.edu.sg/contactus.php" >Feedback</a> |<a href="http://www.nus.edu.sg/campusmap/" >Campus Map</a>
+</br>
+							Last modified on <!-- #BeginDate format:Sw1 -->
+							30 May, 2014<!-- #EndDate -->
+							by Department of Pharmacy
+					</div>
+				</div>
+	
+			<!-- End Footer -->
+		</div>
+	<?php wp_footer();?>
+	</body>
+
+</html>
+			
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
