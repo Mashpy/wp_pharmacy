6fad2147491b1c7c3403777fff2331050329ddf5
mashpy <likewarid@gmail.com>
2014-06-15 16:02:42 +0000

    fixed the bootstrap css problem

--- functions.php
+++ functions.php
@@ -538,6 +538,6 @@ function wpbootstrap_scripts_with_jquery()
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.js', array( 'jquery' ) );
 	// For either a plugin or a theme, you can then enqueue the script:
 	wp_enqueue_script( 'custom-script' );
-  wp_enqueue_style('kadence_bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.css', array('css'));
+  wp_enqueue_style('kadence_bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.css', array());
 }
 add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery' );
